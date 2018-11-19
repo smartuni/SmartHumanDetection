b57474c485ce07420675729d8994145974b5c63a
Alhajras <kharabeash@gmail.com>
2018-11-19 13:57:45 +0100

    cleaning the code and removing the redundant

--- SHD_publisher/main.c
+++ SHD_publisher/main.c
@@ -111,65 +111,14 @@
 #define DELAY (100LU * US_PER_MS)  
 
 //emcute static data
-static char stack4[THREAD_STACKSIZE_DEFAULT];
+
 static msg_t queue[8];
 
 static emcute_sub_t subscriptions[NUMOFSUBS];
 //static char topics[NUMOFSUBS][TOPIC_MAXLEN];
 //
-char stack1[THREAD_STACKSIZE_MAIN];
-char stack2[THREAD_STACKSIZE_MAIN];
-char stack3[THREAD_STACKSIZE_MAIN];
-char roomNumber = '0';
-short roomOneTime = 0;
-short roomTwoTime = 0;
-int roomsTimeArray[2];
-static const uint8_t logo[] = {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0xE0,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F, 0xF8, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x1F, 0xF8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3E, 0x3C,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78, 0x1E, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x70, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x70, 0x0E,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0x0E, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0xF0, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0x1E,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0x3C, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0xF0, 0x7C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x73, 0xF8,
-    0x30, 0x3C, 0x3F, 0xC0, 0x00, 0x0C, 0x77, 0xF0, 0x38, 0x7E, 0x3F, 0xC0,
-    0x00, 0x7E, 0x73, 0xC0, 0x38, 0xE7, 0x06, 0x00, 0x00, 0xFC, 0x71, 0x00,
-    0x38, 0xE3, 0x06, 0x00, 0x01, 0xF0, 0x70, 0x00, 0x38, 0xE3, 0x06, 0x00,
-    0x01, 0xC0, 0x70, 0x00, 0x38, 0xE3, 0x06, 0x00, 0x03, 0x80, 0x70, 0xC0,
-    0x38, 0xE3, 0x06, 0x00, 0x03, 0x80, 0x71, 0xE0, 0x38, 0xE3, 0x06, 0x00,
-    0x03, 0x80, 0x70, 0xE0, 0x38, 0xE3, 0x06, 0x00, 0x03, 0x80, 0x70, 0xF0,
-    0x38, 0xE3, 0x06, 0x00, 0x03, 0x80, 0x70, 0x70, 0x38, 0xE3, 0x06, 0x00,
-    0x03, 0x80, 0xF0, 0x78, 0x38, 0xE3, 0x06, 0x00, 0x03, 0xC1, 0xE0, 0x3C,
-    0x38, 0xE7, 0x06, 0x00, 0x01, 0xE3, 0xE0, 0x3C, 0x38, 0x7E, 0x06, 0x00,
-    0x01, 0xFF, 0xC0, 0x1C, 0x30, 0x3C, 0x06, 0x00, 0x00, 0x7F, 0x80, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00
-};
-
-#if (TEST_OUTPUT == TEST_OUTPUT_I2C) || (TEST_OUTPUT == TEST_OUTPUT_SPI)
-/**
- * @brief   RIOT-OS pin maping of U8g2 pin numbers to RIOT-OS GPIO pins.
- * @note    To minimize the overhead, you can implement an alternative for
- *          u8x8_gpio_and_delay_riotos.
- */
-static gpio_t pins[] = {
-    [U8X8_PIN_CS] = TEST_PIN_CS,
-    [U8X8_PIN_DC] = TEST_PIN_DC,
-    [U8X8_PIN_RESET] = TEST_PIN_RESET
-};
-
-/**
- * @brief   Bit mapping to indicate which pins are set.
- */
-static uint32_t pins_enabled = (
-    (1 << U8X8_PIN_CS) +
-    (1 << U8X8_PIN_DC) +
-    (1 << U8X8_PIN_RESET)
-);
-#endif
+char publisherStack[THREAD_STACKSIZE_MAIN];
+char mainStack[THREAD_STACKSIZE_MAIN];
 
 
 static const char MICRO_CONTROLLER_1[] = "MICRO_CONTROLLER_1";
@@ -272,15 +221,6 @@ static int cmd_pub(int argc, char **argv)
 //
 //void *thread_handler(void *arg);
 
-char* concat(const char *s1, const char *s2)
-{
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
-}
-
 static int raspberry_pi(int sensor1 , int sensor2){
 	
 
@@ -295,7 +235,6 @@ static int raspberry_pi(int sensor1 , int sensor2){
 				// Reading from the infrared sensor  1
 				   else if (sensor1) {
 		                 printf("Movement has been detected in room 1\n");
-						 roomNumber = '1';
 				         gpio_set(GPIO_PIN(0,23));
 						 gpio_clear(GPIO_PIN(0,28));
 						 	char *message[] = {"pub","publisher/roomSensorInformation1/on","1"};
@@ -304,7 +243,6 @@ static int raspberry_pi(int sensor1 , int sensor2){
 				// Reading from the infrared sensor  2  
 		            else if (sensor2) {
 		                 printf("Movement has been detected in room 2\n");
-						 roomNumber = '2';
 						 gpio_set(GPIO_PIN(0,28));
 				         gpio_clear(GPIO_PIN(0,23));
 						 char *message[] = {"pub","publisher/roomSensorInformation2","1"};
@@ -378,197 +316,7 @@ void *adc(void *argv){
 return NULL;;
 }
 
-void *timeLEDIsOn(void *arg)
-{
-	(void) arg;
-	  while(1){
-		  if(roomNumber == '1'){
-			  xtimer_sleep(1);
-			  roomsTimeArray[0]++;
-		  }
-			  else if(roomNumber == '2'){
-				  xtimer_sleep(1);
-				  roomsTimeArray[1]++;
-				  
-			  }
-			  
-			  else{
-				  xtimer_sleep(1);	  
-			  }
-	     
-		  printf("Room 1 was on for:  %d \n", roomsTimeArray[0]);
-		  printf("Room 2 was on for:  %d \n", roomsTimeArray[1]);
-	  }
-	    return 0;
-}
-
-
-
-short powerConsumtion(void){
-
-	if(roomNumber == '1'){
-		return (roomsTimeArray[0] * 100/ (roomsTimeArray[0] + roomsTimeArray[1]));
-	}
-	else if(roomNumber == '2'){
-		return (roomsTimeArray[1] * 100/ (roomsTimeArray[0] + roomsTimeArray[1]));
-	}
-	return 0; 
-}
-
-char * append(char * string1, char * string2)
-{
-    char * result = NULL;
-    asprintf(&result, "%s%s", string1, string2);
-    return result;
-}
-
-void *monitorThread(void *arg)
-{
-	(void) arg;
-    uint32_t screen = 0;
-    u8g2_t u8g2;
-
-	    
-					
-    /* initialize to stdout */
-#if TEST_OUTPUT == TEST_OUTPUT_STDOUT
-    puts("Initializing to stdout.");
-
-    u8g2_SetupBuffer_Utf8(&u8g2, U8G2_R0);
-#endif
-
-    /* initialize to virtual SDL (native only) */
-#if TEST_OUTPUT == TEST_OUTPUT_SDL
-    puts("Initializing to SDL.");
-
-    u8g2_SetupBuffer_SDL_128x64_4(&u8g2, U8G2_R0);
-#endif
-
-    /* initialize to SPI */
-#if TEST_OUTPUT == TEST_OUTPUT_SPI
-    puts("Initializing to SPI.");
-
-    TEST_DISPLAY(&u8g2, U8G2_R0, u8x8_byte_riotos_hw_spi, u8x8_gpio_and_delay_riotos);
-
-    u8g2_SetPins(&u8g2, pins, pins_enabled);
-    u8g2_SetDevice(&u8g2, SPI_DEV(TEST_SPI));
-#endif
-
-    /* initialize to I2C */
-#if TEST_OUTPUT == TEST_OUTPUT_I2C
-    puts("Initializing to I2C.");
-
-    TEST_DISPLAY(&u8g2, U8G2_R0, u8x8_byte_riotos_hw_i2c, u8x8_gpio_and_delay_riotos);
-
-    u8g2_SetPins(&u8g2, pins, pins_enabled);
-    u8g2_SetDevice(&u8g2, I2C_DEV(TEST_I2C));
-    u8g2_SetI2CAddress(&u8g2, TEST_ADDR);
-#endif
-
-    /* initialize the display */
-    puts("Initializing display.");
-
-    u8g2_InitDisplay(&u8g2);
-    u8g2_SetPowerSave(&u8g2, 0);
-
-    /* start drawing in a loop */
-    puts("Drawing on screen.");
-    char message[] = "Room:  ";
-    while (1) {
-        u8g2_FirstPage(&u8g2);
-
-        do {
-            u8g2_SetDrawColor(&u8g2, 1);
-            u8g2_SetFont(&u8g2, u8g2_font_helvB12_tf);
-
-            switch (screen) {
-                case 0:
-                    u8g2_DrawStr(&u8g2, 5, 22, "Someone");
-					u8g2_DrawStr(&u8g2, 5, 40, "in:");
-                    
-                    break;
-                case 1:
-                    message[6] = roomNumber;
-                    u8g2_DrawStr(&u8g2, 5, 22, message);
-					char str[255];
-					short powerConsumtionVar = powerConsumtion();
-                    sprintf(str, "%d", powerConsumtionVar);
-				/*	char result[255];
-					 short a;
-					 printf("the number is %s",str);
-					 for( a = 0; a <= 5; a++ ){
-						 printf("the number is %d",powerConsumtionVar/10);
-						 if(a == 2 && str[0]!='1' && str[1]!='0' && str[2]!='0')
-							 result[a] = '.';
-						 else if(a == 3 && str[0]=='1' && str[1]=='0' && str[2]=='0')
-							 result[a] = '.';
-							 else
-						result[a]= str[a];
-					
-						}
-						printf("the number after is %s",result);
-					//strcpy(result, str);*/
-                    u8g2_DrawStr(&u8g2, 5, 40, append(str, "%"));
-		break;
-                case 2:
-                    u8g2_DrawBitmap(&u8g2, 0, 0, 8, 32, logo);
-                       
-		break;
-            }
-        } while (u8g2_NextPage(&u8g2));
-
-#if TEST_OUTPUT == TEST_OUTPUT_STDOUT
-        /* transfer screen buffer to stdout */
-        utf8_show();
-#endif
-
-#if TEST_OUTPUT == TEST_OUTPUT_SDL
-        /* handle SDL events */
-        u8g_sdl_get_key();
-#endif
-
-        /* show screen in next iteration */
-        screen = (screen + 1) % 3;
-
-        /* sleep a little */
-        xtimer_sleep(3);
-    }
-
-    return 0;
-}
-
-
 void functionMCSensor (void){
-	 /* the main thread needs a msg queue to be able to run `ping6`*/
-    msg_init_queue(queue, (sizeof(queue) / sizeof(msg_t)));
-
-    /* initialize our subscription buffers */
-    memset(subscriptions, 0, (NUMOFSUBS * sizeof(emcute_sub_t)) );
-
-    /* start the emcute thread */
-    thread_create(stack4, sizeof(stack4), EMCUTE_PRIO, 0,
-                  emcute_thread, NULL, "emcute");
-
-		    char *args[] = {"con",BROCKER_IP,BROCKER_PORT,"",""};				
-					 if(!cmd_con(5, args)){
-						 puts("connection has been established");
-					char *message[] = {"pub","publisher/communication", "\"The SHD microcontroller is live, and ready to communicate.\""};
-						 cmd_pub(3, message);
-            thread_create(stack2, sizeof(stack2),
-                    THREAD_PRIORITY_MAIN - 1,
-                    THREAD_CREATE_STACKTEST,
-                    adc,
-                    NULL, "mainThread");
-					 }else{
-						 puts("error in connection!!!");
-				
-					 }
-
-					return ;
-}
-
-void functionMCController (void){
-	
 	
 	// LED 1 OUTPUT
     if (gpio_init(GPIO_PIN(0, 23),  GPIO_OUT) < 0) {
@@ -583,28 +331,40 @@ void functionMCController (void){
 	
 	    // Turn off the LED 1 as first state
 	    gpio_clear(GPIO_PIN(0,23)); 
-	    gpio_clear(GPIO_PIN(0,28)); 
-	
-		  thread_create(stack1, sizeof(stack1),
+gpio_clear(GPIO_PIN(0,28)); 
+
+
+	 /* the main thread needs a msg queue to be able to run `ping6`*/
+    msg_init_queue(queue, (sizeof(queue) / sizeof(msg_t)));
+
+    /* initialize our subscription buffers */
+    memset(subscriptions, 0, (NUMOFSUBS * sizeof(emcute_sub_t)) );
+
+    /* start the emcute thread */
+    thread_create(publisherStack, sizeof(publisherStack), EMCUTE_PRIO, 0,
+                  emcute_thread, NULL, "emcute");
+
+		    char *args[] = {"con",BROCKER_IP,BROCKER_PORT,"",""};				
+					 if(!cmd_con(5, args)){
+						 puts("connection has been established");
+					char *message[] = {"pub","publisher/communication", "\"The SHD microcontroller is live, and ready to communicate.\""};
+						 cmd_pub(3, message);
+            thread_create(mainStack, sizeof(mainStack),
                     THREAD_PRIORITY_MAIN - 1,
                     THREAD_CREATE_STACKTEST,
-                    monitorThread,
-                    NULL, "monitorThread");
-					
-		  thread_create(stack3, sizeof(stack3),
-                    THREAD_PRIORITY_MAIN - 1,
-                    THREAD_CREATE_STACKTEST,
-                    timeLEDIsOn,
-                    NULL, "timeLEDIsOn");
-					
+                    adc,
+                    NULL, "mainThread");
+					 }else{
+						 puts("error in connection!!!");
+				
+					 }
+
 					return ;
 }
 
 int main(void)
 {
-	functionMCSensor(); // function to simulate sensor mc
-	functionMCController(); // function to simulate the controller mc
-		
+	functionMCSensor(); // function to simulate sensor mc		
     return 0;
 }
 
